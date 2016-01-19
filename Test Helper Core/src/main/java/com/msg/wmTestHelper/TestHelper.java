ee0567b37e5623dc139440123ed87221ae14c541
Dennis Rippinger <dennis.rippinger@gmail.com>
2016-01-19 23:28:24 +0100

    Header added

--- Test Helper Core/src/main/java/com/msg/wmTestHelper/TestHelper.java
+++ Test Helper Core/src/main/java/com/msg/wmTestHelper/TestHelper.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2016 Dennis Rippinger (dennis.rippinger@msg-systems.com)
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,6 +32,8 @@ import java.util.Collection;
 public class TestHelper {
 
 	private FileSearch fileSearch = new FileSearch();
+
+
 
 	/**
 	 * Generates a set of classes depending on the WM input files.
